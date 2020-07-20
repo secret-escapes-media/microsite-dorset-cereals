59e251fe380b5b8f9bc9cd5db317519763fadc66
hamishgray <hamish.gray@secretescapes.com>
2020-07-20 15:51:58 +0100

    setup site for dynamic offer sheet

--- _assets/js/_components/offer-sheet.js
+++ _assets/js/_components/offer-sheet.js
@@ -31,6 +31,7 @@ $(offerSections).each(function(){
     var sortedDeals = deals.sort(function(a,b){ return a[6] > b[6] ?1 :-1 });
 
     // loop through deals and build HTML for output
+    offerSection.empty(); // remove loading icon
     $(deals).each(function(i){
       // check deal "end date" to ensure it exists
       if(this[7]){
@@ -47,7 +48,7 @@ $(offerSections).each(function(){
         var saleURL               = this[9];
         var saleImage             = this[10];
 
-        offerSection.prepend('<div class="col"><div class="offer"><a class="offer__link" href="'+saleURL+'"><span class="btn btn--orange">View Offer</span></a><div class="bg-img bg-img--3-2" style="background-image: url('+saleImage+')"></div><div class="offer__content"><h3 class="title title--xs">'+saleTitle+'</h3><h4 class="offer__location">'+saleLocation+'</h4><div class="offer__description">'+saleDescription+'</div><div class="offer__bottom"><div class="offer__bottom-left"><div class="offer__saved"><div class="offer__saved-label">Save up to&nbsp;</div><div class="offer__saved-percent">'+saleDiscount+'%</div></div><div class="offer__separator"></div><div class="offer__details"><div>From <span class="offer__price">£'+salePrice+'</span></div><div class="text--sm">'+salePriceDescription+'</div><div class="js-offer-expires text--sm" data-expires="'+saleEndDate+'"></div></div></div><div class="offer__bottom-right"><div class="offer__arrow"></div></div></div></div></div></div>');
+        offerSection.prepend('<div class="col"><div class="offer"><a class="offer__link" href="'+saleURL+'"><span class="btn btn--orange">View Offer</span></a><div class="bg-img bg-img--3-2" style="background-image: url('+saleImage+')"></div><div class="offer__content"><h3 class="title title--xs">'+saleTitle+'</h3><h4 class="offer__location">'+saleLocation+'</h4><div class="offer__description">'+saleDescription+'</div><div class="offer__bottom"><div class="offer__bottom-left"><div class="offer__saved"><div class="offer__saved-label">Save up to&nbsp;</div><div class="offer__saved-percent">'+saleDiscount+'%</div></div><div class="offer__separator"></div><div class="offer__details"><div>From <span class="offer__price">'+salePrice+'</span></div><div class="text--sm">'+salePriceDescription+'</div></div></div><div class="offer__bottom-right"><div class="offer__arrow"></div></div></div></div></div></div>');
       }// end if
     });// end forloop of deals
   });// end JSON request
